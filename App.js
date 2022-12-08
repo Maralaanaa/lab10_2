0efe56517b8804803e7bfbd60575364d9ba0e5a6
Maralaanaa <gansukh.mapalmaa@gmail.com>
2022-12-08 18:06:31 +0800

    B200910040

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
+import React, { Component } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+export default class ButtonBasics extends Component { _onPressButton() {
+  alert('You tapped the button!') }
+  render() {
+    return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <View style={styles.buttonContainer}>
+        <Button
+        onPress={this._onPressButton}
+        title="Press Me"/>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+          onPress={this._onPressButton}
+          title="Press Me"
+          color="#841584"/> 
+          </View>
+          <View style={styles.alternativeLayoutButtonContainer}> 
+          <Button 
+          onPress={this._onPressButton}
+          title="This looks great!"/>
+          <Button
+          onPress={this._onPressButton}
+          title="OK!"
+          color="#841584"/> 
+          </View>
+          </View>
+          );
+        } 
+      }
+      const styles = StyleSheet.create({ 
+        container: {
+          flex: 1,
+          justifyContent: 'center',
+        },
+        buttonContainer: {
+          margin: 20
+        }, 
+        alternativeLayoutButtonContainer: {
+          margin: 20,
+          flexDirection: 'row', 
+          justifyContent: 'space-between'
+        } 
+      });
